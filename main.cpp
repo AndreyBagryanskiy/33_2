f3544e0905df60d66ffe18d551a985e336a3a1a7
Багрянский Андрей <andrey@1dist.ru>
2023-10-24 00:17:12 +0300

    1

--- main.cpp
+++ main.cpp
@@ -6,21 +6,21 @@
 class BootCatch: public std::exception  {
  public:
     const char* what() const noexcept override{
-        return "You've catch the boot!\n";
+        return "You've catch the boot!";
     }
 };
 
 class FishCatch:  public std::exception {
  public:
     const char* what() const noexcept override{
-        return "You've caught the fish!\n";
+        return "You've caught the fish!";
     }
 };
 
 class NoCatch:  public std::exception {
 public:
     const char* what() const noexcept override{
-        return "You've catch nothing! Try again!\n";
+        return "You've catch nothing! Try again!";
     }
 }; 
 
@@ -45,11 +45,39 @@ int main(){
             ++count;
         }
     }
-    printField(field);
-    std::cout <<" ";
+    //printField(field);
+    while(true){
+        std::cout << "Input the sector number (0 - 8) to cast the fishing rod: ";
+        int number;
+        try{
+            std::cin >> number;
+            if(std::cin.fail() || number < 0 || number > 8){ 
+                std::cin.clear();
+                std::cin.sync();
+                throw std::invalid_argument("Invalid number sector, repeat input!");
+            }
+            if(field[number] == No){
+                throw NoCatch();
+            }
+            else if(field[number] == Boot){
+                throw BootCatch();
+            }else{
+                throw FishCatch();
+            }          
+        }catch(const std::invalid_argument& e){
+            std::cerr << e.what() << std::endl;
+        }
+        catch(const NoCatch& e){
+            std::cerr << e.what() << std::endl;
+        }catch(const BootCatch& e){
+            std::cerr << e.what() << std::endl;
+            break;
+        }catch(const FishCatch& e){
+            std::cerr << e.what() << std::endl;
+            break;
+        }
 
-
-
+    }
 }
 
 
